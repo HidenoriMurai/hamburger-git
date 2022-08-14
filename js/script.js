cd57803162ce14fc940a4798bbb562b6b04ccfda
Hidenori Murai <hidenoweb822@gmail.com>
2022-08-14 10:31:22 +0900

    クラス名調整

--- js/script.js
+++ js/script.js
@@ -14,27 +14,27 @@ $(document).on('click',function(e) {
 //オーバーレイ：フェードイン
 $(function() {
     // 「検索」ボタンクリック
-    $(".c-button--menu").on('click', function() {
+    $(".js-button--menu").on('click', function() {
         $(".overlay").fadeIn();
     });
 });
 //　　　　　　：フェードアウト
 $(function() {
     // 「バツ」ボタンクリック
-    $(".c-btn__close").on('click', function() {
+    $(".js-btn__close").on('click', function() {
         $(".overlay").fadeOut();
     });
 });
 
 //スライドメニュー
-$(".c-button--menu").click(function () {//ボタンがクリックされたら
+$(".js-button--menu").click(function () {//ボタンがクリックされたら
     $(".p-aside").toggleClass('p-aside-open');//クラスを付与
-    $(".f-body").toggleClass('f-body_scroll');//クラスを付与
+    $(".l-body").toggleClass('l-body_scroll');//クラスを付与
 });
 
 
-$(".c-btn__hamburger").click(function () {//クリックされたら
+$(".js-btn__hamburger").click(function () {//クリックされたら
     $(".p-aside").removeClass('p-aside-open');//クラスも除去
-    $(".f-body").removeClass('f-body_scroll');//クラスも除去
+    $(".l-body").removeClass('l-body_scroll');//クラスも除去
 });
 
