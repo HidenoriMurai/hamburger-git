d2a38af6ee8c5bcb94441a92e0fbbe281cdbc84a
Hidenori Murai <hidenoweb822@gmail.com>
2022-08-19 12:25:39 +0900

    サイドバー表示位置修正

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ $(function(){
         $(".p-header__search-icon1").hide();
     });
 });
+
 //虫眼鏡の表示
 $(document).on('click',function(e) {
     if(!$(e.target).closest('.p-header__search').length) {
