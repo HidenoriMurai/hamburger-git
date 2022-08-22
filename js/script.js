83dd0bade7c5b34aaca6cc8c600a0d4941a80895
Hidenori Murai <hidenoweb822@gmail.com>
2022-08-22 13:36:18 +0900

    修正

--- js/script.js
+++ js/script.js
@@ -1,17 +1,3 @@
-//虫眼鏡の非表示
-$(function(){
-    $(".js-header__search").on("click",function(){
-        $(".p-header__search-icon1").hide();
-    });
-});
-
-//虫眼鏡の表示
-$(document).on('click',function(e) {
-    if(!$(e.target).closest('.p-header__search').length) {
-        $(".p-header__search-icon1").show();
-    } 
-});
-
 //オーバーレイ：フェードイン
 $(function() {
     // 「検索」ボタンクリック
